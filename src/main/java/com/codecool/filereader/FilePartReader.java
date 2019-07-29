8011746dbefb5541b51b5d6793e14f5bc63c52c2
Szanyi Krisztián <szkript@gmail.com>
2019-07-29 15:06:48 +0200

    init

---
+++ src/main/java/com/codecool/filereader/FilePartReader.java
@@ -0,0 +1,14 @@
+package com.codecool.filereader;
+
+public class FilePartReader {
+    public void setup(String filePath, Integer fromLine, Integer toLine) {
+    }
+
+    public String read() {
+        return null;
+    }
+
+    public String readLines() {
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/filereader/FileWordAnalyzer.java
@@ -0,0 +1,21 @@
+package com.codecool.filereader;
+
+import java.util.List;
+
+public class FileWordAnalyzer {
+    FilePartReader fpr = new FilePartReader();
+
+    public List getWordsOrderedAlphabetically() {
+        fpr.readLines();
+        return null;
+    }
+    public List getWordsContainingSubstring(String subString){
+        fpr.readLines();
+        return null;
+    }
+    public  List getStringWichPalindromes(){
+        fpr.readLines();
+        return null;
+    }
+
+}
